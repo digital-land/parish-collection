26e18ff0516da80da3cbbe6d1c57277ff6272f4d
Jon Bramley <brammers@gmail.com>
2021-02-12 14:10:17 +0000

    fix: need geospatial.mk to install gdal

---
+++ makerules/geospatial.mk
@@ -0,0 +1,14 @@
+GDAL := $(shell command -v ogr2ogr 2> /dev/null)
+UNAME := $(shell uname)
+
+init::
+ifndef GDAL
+ifeq ($(UNAME),Darwin)
+$(error GDAL tools not found in PATH)
+endif
+	sudo apt-get update
+	sudo apt-get install gdal-bin
+endif
+
+makerules::
+	curl -qsL '$(SOURCE_URL)/makerules/main/geospatial.mk' > makerules/geospatial.mk
